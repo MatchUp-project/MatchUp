f64aee1f0339798064c2da34ba89ad3654ffe6f6
chaji <chaj1684@gmail.com>
2025-11-11 16:40:20 +0900

    develop 브렌치 생성

--- src/main/java/com/team10/matchup/MatchupApplication.java
+++ src/main/java/com/team10/matchup/MatchupApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MatchupApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MatchupApplication.class, args);
+        SpringApplication.run(MatchupApplication.class, args);
 	}
-
 }
